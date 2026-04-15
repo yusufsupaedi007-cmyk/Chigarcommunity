6deaa39ac2bf32435fa04cba549ce2aea8872899
ChigarCommunity <yusufsupaedi007@gmail.com>
2026-04-16 04:38:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ app.use(require('cors')());
 app.use(express.json());
 let donations = [];
 app.post('/webhook', (req, res) => {
-  const data = req.body.data || req.body;
-
-  donations.unshift({
+  const data = req.body;
+  const donation = {
     name: data.donator_name || 'Anonim',
-    amount: Number(data.amount) || 0,
+    amount: Number(data.amount_to_display) || 0,
     message: data.message || ''
-  });
+  };
+  console.log("DONASI MASUK:", donation);
+  donations.unshift(donation);
+  if (donations.length > 20) donations.pop();
+  res.json({ ok: true });
+});
   if (donations.length > 20) donations.pop();
   console.log("DONASI MASUK:", data); // debug
   res.json({ ok: true });
