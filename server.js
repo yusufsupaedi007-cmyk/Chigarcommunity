70e857f6a188be614816b5c000ef23b2c5395ec2
ChigarCommunity <yusufsupaedi007@gmail.com>
2026-04-16 04:58:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ app.use(require('cors')());
 app.use(express.json());
 let donations = [];
 app.post('/webhook', (req, res) => {
-  const {donator_name,amount,message} = req.body;
-  donations.unshift({name:donator_name||'Anonim',
-    amount:amount||0,message:message||''});
-  if(donations.length>20)donations.pop();
-  res.json({ok:true});
+  console.log("DATA:", req.body);
+
+  const { donator_name, amount_raw, message } = req.body;
+
+  donations.unshift({
+    name: donator_name || 'Anonim',
+    amount: Number(amount_raw) || 0,
+    message: message || ''
+  });
+
+  if (donations.length > 20) donations.pop();
+
+  res.json({ ok: true });
 });
 app.get('/donations',(req,res)=>{
   res.json(donations);
